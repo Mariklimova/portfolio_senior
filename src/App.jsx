c7ffd6ad652f000e136d8007ea49069a71a83ab1
Mariklimova <mariku22@yandex.ru>
2024-12-14 11:44:45 +0300

    working with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Projects from './pages/Projects/Projects'
+import Contacts from './pages/Contacts/Contacts'
 
 export default function App() {
 
   return <>
-    <h1>Hi</h1>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/projects' element={<Projects/>}></Route>
+      <Route path='/contacts' element={<Contacts/>}></Route>
+    </Routes>
   </>
 
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import style from './home.module.css'
+
+export default function Home() {
+    return <>
+     <h1>Elias is a web designer and front-end developer</h1>
+    </>
+}
